8845780ce2bc09558af95478d1088bf4e95c091c
liyitong <yitong.li@wmeimob.com>
2019-07-11 18:42:32 +0800

    fix:taro.d.ts文件tslint问题

--- src/types/taro.d.ts
+++ src/types/taro.d.ts
@@ -1,32 +1,30 @@
 declare namespace Taro {
 
     interface NavigateToArguments extends navigateTo.Param {
-        params?: { [key: string]: any }
+        params?: { [key: string]: any };
     }
 
     interface RedirectToArguments extends redirectTo.Param {
-        params?: { [key: string]: any }
+        params?: { [key: string]: any };
     }
 
     interface SwitchTabArguments extends switchTab.Param {
-        params?: { [key: string]: any }
+        params?: { [key: string]: any };
     }
 
     interface NavigateBackArguments extends navigateBack.Param {
-        params?: { [key: string]: any }
+        params?: { [key: string]: any };
     }
 
     interface ReLaunchArguments extends reLaunch.Param {
-        params?: { [key: string]: any }
+        params?: { [key: string]: any };
     }
 
-    function navigateTo(parameter: NavigateToArguments): Promise<any>
-    function redirectTo(parameter: RedirectToArguments): Promise<any>
-    function switchTab(parameter: SwitchTabArguments): Promise<any>
-    function navigateBack(parameter: NavigateBackArguments): Promise<any>
-    function reLaunch(parameter: ReLaunchArguments): Promise<any>
+    function navigateTo(parameter: NavigateToArguments): Promise<any>;
+    function redirectTo(parameter: RedirectToArguments): Promise<any>;
+    function switchTab(parameter: SwitchTabArguments): Promise<any>;
+    function navigateBack(parameter: NavigateBackArguments): Promise<any>;
+    function reLaunch(parameter: ReLaunchArguments): Promise<any>;
 
 }
-
-
 
