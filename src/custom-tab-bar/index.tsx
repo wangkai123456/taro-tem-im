c17e80305b32f53f09ccb257f3419c6b54017eb2
yitong.li <yitong.li@weimob.com>
2020-04-26 13:30:49 +0800

    feat:删除自定义导航

--- src/custom-tab-bar/index.tsx
+++ src/custom-tab-bar/index.tsx
@@ -3,25 +3,7 @@ import IconFontName from '~/modules/@wmeimob/weapp-design/src/components/icon-fo
 import MMTabBar from '~/modules/@wmeimob/weapp-design/src/components/tab-bar';
 
 export default class Index extends Component {
-    static data = [
-        {
-            iconfont: IconFontName.Folder,
-            text: '基础',
-            url: '/pages/template/index/index'
-        },
-        {
-            iconfont: IconFontName.Send,
-            text: '请求',
-            url: '/pages/template/request/index'
-        },
-        {
-            iconfont: IconFontName.Class,
-            text: '其他',
-            url: '/pages/template/other/index'
-        }
-    ];
-
     render() {
-        return (<MMTabBar defaultData={Index.data}></MMTabBar>);
+        return (<MMTabBar />);
     }
 }
