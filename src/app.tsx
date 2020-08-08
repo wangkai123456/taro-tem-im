2ed513611abbd5363fc6e0f9605e060b6ccfc161
yitong.li <yitong.li@weimob.com>
2020-08-08 16:56:11 +0800

    feat: 添加配置文件

--- src/app.tsx
+++ src/app.tsx
@@ -14,74 +14,74 @@ import pagesIndexStore from './pages/template/index/store';
 // }
 
 const store = {
-    global,
-    pagesIndexStore
+  global,
+  pagesIndexStore
 };
 
 class App extends Component {
-    /**
-     * 指定config的类型声明为: Taro.Config
-     *
-     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-     */
-    config: Config = {
-        pages: [
-            'pages/template/index/index',
-            'pages/template/other/index',
-            'pages/template/mobx/index',
-            'pages/template/login/index',
-            'pages/template/test/index',
-            'pages/template/decorator/index',
-            'pages/template/transparent/index',
-            'pages/template/request/index'
-        ],
-        tabBar: {
-            custom: true,
-            list: [{
-                'pagePath': 'pages/template/index/index',
-                'text': '首页'
-            }, {
-                'pagePath': 'pages/template/request/index',
-                'text': '请求'
-            }, {
-                'pagePath': 'pages/template/other/index',
-                'text': '其他'
-            }]
-        },
-        window: {
-            // 默认颜色不显示下拉加载的loading图片
-            backgroundTextStyle: 'dark',
-            navigationBarBackgroundColor: '#fff',
-            navigationBarTitleText: 'WeChat',
-            navigationBarTextStyle: 'black'
-        }
-    };
-
-    globalData = {
-        userInfo: null
-    };
-
-    /**
-     * 程序初始化
-     *
-     * @memberof App
-     */
-    constructor() {
-        super();
-        mount();
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    pages: [
+      'pages/template/index/index',
+      'pages/template/other/index',
+      'pages/template/mobx/index',
+      'pages/template/login/index',
+      'pages/template/test/index',
+      'pages/template/decorator/index',
+      'pages/template/transparent/index',
+      'pages/template/request/index'
+    ],
+    tabBar: {
+      custom: true,
+      list: [{
+        'pagePath': 'pages/template/index/index',
+        'text': '首页'
+      }, {
+        'pagePath': 'pages/template/request/index',
+        'text': '请求'
+      }, {
+        'pagePath': 'pages/template/other/index',
+        'text': '其他'
+      }]
+    },
+    window: {
+      // 默认颜色不显示下拉加载的loading图片
+      backgroundTextStyle: 'dark',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
     }
+  };
 
-    // 在 App 类中的 render() 函数没有实际作用
-    // 请勿修改此函数
-    render() {
-        return (
-            <Provider store={store}>
-                <Index />
-            </Provider>
-        );
-    }
+  globalData = {
+    userInfo: null
+  };
+
+  /**
+   * 程序初始化
+   *
+   * @memberof App
+   */
+  constructor() {
+    super();
+    mount();
+  }
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render() {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    );
+  }
 }
 
 Taro.render(<App />, document.getElementById('app'));
